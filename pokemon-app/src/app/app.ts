6790cf848f3ca4c4a6123648a0bbcee9524cb018
Leslie <LeslieMCunningham@outlook.com>
2026-02-25 17:33:42 -0500

    Some Format Changes

--- pokemon-app/src/app/app.ts
+++ pokemon-app/src/app/app.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
 
+import { pokeService } from './app.service';
 import { Header } from '../header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Header],
+  imports: [RouterOutlet, RouterLink, Header, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pokemon-app');
+  // Get PService
+  Pservice = inject(pokeService);
+
+  
 }
